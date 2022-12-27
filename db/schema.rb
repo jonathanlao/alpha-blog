db287a7b2aa1ab210763ae4654a5db6a2f048b67
Jonathan Lao <jlao@justworks.com>
2022-12-27 16:36:20 -0500

    add bcrypt gem. Add password field to User table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_27_210342) do
+ActiveRecord::Schema.define(version: 2022_12_27_212720) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_12_27_210342) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
